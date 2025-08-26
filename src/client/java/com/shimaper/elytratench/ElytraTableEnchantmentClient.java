dcd9be6a201c702f10f1a6094aff54410c941fac
sourcels <siperdima3@gmail.com>
2025-08-26 13:08:52 +0200

    Init

---
+++ src/client/java/com/shimaper/elytratench/ElytraTableEnchantmentClient.java
@@ -0,0 +1,10 @@
+package com.shimaper.elytratench;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElytraTableEnchantmentClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
